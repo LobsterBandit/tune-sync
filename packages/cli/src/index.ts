6b73f57d76c5592f169d28d20a48623f7b2f1fb1
lobsterbandit <code.lobsterbandit@outlook.com>
2019-08-24 15:38:41 -0400

    Merge branch scaffold-cli:
    commit ac3e67c45659c1a2fadc6d7ccbd40601334a65d8
    Author: lobsterbandit <code.lobsterbandit@outlook.com>
    Date:   Sat Aug 24 15:36:19 2019 -0400
    
        Add a user command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,18 @@
-import { pandora } from './plugins';
+/* eslint-disable no-console */
+import * as fs from 'fs';
+import * as path from 'path';
+import yargs from 'yargs';
+import { PandoraUser } from '@favtunes/pandora';
 
-pandora();
+const argv = yargs
+  .command('user [key]', 'show user profile', {}, () => {
+    const user = JSON.parse(
+      fs.readFileSync(path.join(__dirname, '../pandorauser.json'), 'utf8'),
+    ) as PandoraUser;
+    console.log('The User Profile:');
+    console.log(JSON.stringify(user, null, 2));
+  })
+  .demandCommand()
+  .help().argv;
+
+console.log(argv);
